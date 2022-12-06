3e424b086949339114e1bf2247e2191e61da334a
Yaroslav Shybinskyi <customsbroker93@gmail.com>
2022-12-06 11:34:23 +0200

    practice(part 3)

--- js/script.js
+++ js/script.js
@@ -23,7 +23,417 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// Homework 1
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', '');
+// const b = prompt('На сколько оцените его?', '');
+// const c = prompt('Один из последних просмотренных фильмов?', '');
+// const d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+// Tasks logical operators
+
+// 1.
+
+// console.log(NaN || 2 || undefined); // 2
+
+// console.log(NaN && 2 && undefined); // NaN
+
+// console.log(1 && 2 && 3); // 3
+
+// console.log((!1 && 2) || !3); // false (!3)
+
+// console.log(25 || (null && !3)); // 25
+
+// console.log(NaN || null || !3 || undefined || 5); // 5
+
+// console.log(NaN || (null && !3 && undefined) || 5); // 5
+
+// console.log((5 === 5 && 3 > 1) || 5); // true
+
+// 2.
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if ((hamburger === 3 && cola) || (fries === 3 && nuggets)) {
+//   console.log('Done!');
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//   console.log('Done!');
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if ((hamburger && cola) || (fries === 3 && nuggets)) {
+//   console.log('Done!');
+// }
+
+// Задания на использование циклов и условий
+
+// 1.При помощи цикла выведите числа от 5 до 10 в консоль. 5 и 10 включительно. Цикл можно использовать любой
+
+// function firstTask() {
+//   // Пишем решение вот тут
+//   for (let i = 5; i <= 10; i++) {
+//     console.log(i);
+//   }
+// }
+// firstTask();
+
+// 2.При помощи цикла for вывести числа от 20 до 10 в консоль. В обратном порядке (20, 19, 18...). Когда цикл дойдет до числа 13 - остановить весь цикл
+
+// function secondTask() {
+//   // Пишем решение вот тут
+//   for (let i = 20; i >= 10; i--) {
+//     if (i === 13) {
+//       break;
+//     }
+//     console.log(i);
+//   }
+// }
+// secondTask();
+
+// 3. При помощи цикла for выведите чётные числа от 2 до 10 включительно
+
+// function thirdTask() {
+//   // Пишем решение вот тут
+//   for (let i = 2; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//       console.log(i);
+//     }
+//   }
+// }
+// thirdTask();
+
+// 4. Перепишите цикл  for на вариант с while. Результат должен остаться точно таким же. Не создайте бесконечный цикл! Иначе браузер может зависнуть.
+
+// for (let i = 2; i <= 16; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// function fourthTask() {
+//   let i = 2;
+
+//   while (i <= 16) {
+//     if (i % 2 === 0) {
+//       i++;
+//       continue;
+//     } else {
+//       console.log(i);
+//     }
+//     i++;
+//   }
+// }
+// fourthTask();
+
+// 5. Заполните массив цифрами от 5 до 10 включительно. Помните, что элементы массива можно сформировать так же, как и обращаться к ним: arr[0]
+
+// function fifthTask() {
+//   const arrayOfNumbers = [];
+
+//   // Пишем решение вот тут
+//   for (let i = 5; i < 11; i++) {
+//     arrayOfNumbers.push(i);
+//   }
+//   // Не трогаем
+//   return arrayOfNumbers;
+// }
+// console.log(fifthTask());
+
+// (*) Продвинутые задания на использование циклов и условий
+
+// 1. Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд. Должен получиться точно такой же массив.
+
+// function firstTask() {
+//   // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//   const arr = [3, 5, 8, 16, 20, 23, 50];
+//   const result = [];
+
+//   // Пишем решение вот тут
+//   for (let i = 0; i <= arr.length; i++) {
+//     result[i] = arr[i];
+//   }
+//   console.log(result);
+//   //   // Не трогаем
+//   return result;
+// }
+// firstTask();
+
+// 2. Измените данный массив так, чтобы все числа были увеличены в 2 раза, а если попадается строка строка - то к ней было добавлено " - done".
+// Для определения типа данных используйте typeof();
+// Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
+
+// function secondTask() {
+//   // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//   const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+//   // Пишем решение вот тут
+
+//   for (let i = 0; i < data.length; i++) {
+//     if (typeof data[i] == 'number') {
+//       data[i] *= 2;
+//     } else if (typeof data[i] == 'string') {
+//       data[i] += ' - done';
+//     }
+//   }
+//   console.log(data);
+//   // Не трогаем
+//   return data;
+// }
+// console.log(secondTask());
+
+// 3. Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
+// Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
+
+// function thirdTask() {
+//   // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//   const data = [5, 10, 'Shopping', 20, 'Homework'];
+//   const result = [];
+
+//   // Пишем решение вот тут
+//   for (let i = 1; i <= data.length; i++) {
+//     result[i - 1] = data[data.length - i];
+//   }
+//   console.log(result);
+//   // Не трогаем
+//   return result;
+// }
+// thirdTask();
+
+// Задача на формирование фигуры
+// 1. Сейчас вам необходимо написать код, который при помощи звездочек (*) в консоли нарисует вот такую фигуру:
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+// (Подсказка: в конце фигуры есть перенос строки \n, который тоже учитывается в тестах. В КОНЦЕ КАЖДОЙ СТРОКИ НЕТ ПРОБЕЛОВ, ТОЛЬКО ПЕРЕНОС)
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 1; i < lines; i++) {
+//   for (let j = 0; j < i; j++) {
+//     result += '*';
+//   }
+//   result += '\n';
+// }
+
+// console.log(result);
+
+// Homework 2
+
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// Код возьмите из предыдущего домашнего задания
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
+
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt('Один из последних просмотренных фильмов?', '');
+//   const b = prompt('На сколько оцените его?', '');
+
+//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log('done !');
+//   } else {
+//     console.log('error !');
+//     i--;
+//   }
+// }
+
+// if (personalMovieDB.count < 10) {
+//   console.log('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+//   console.log('Вы классический зритель');
+// } else if (personalMovieDB.count > 30) {
+//   console.log('Вы киноман');
+// } else {
+//   console.log('Произошла ошибка');
+// }
+// console.log(personalMovieDB);
+
+// Практика
+
+// 1. Задача
+
+// const usdCurr = 28;
+// const eurCurr = 32;
+
+// function convert(amount, curr) {
+//   console.log(curr * amount);
+// }
+// convert(500, usdCurr);
+// convert(500, eurCurr);
+
+// 2. Задача (Про важность return)
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//   return curr * amount;
+// }
+
+// function promotion(result) {
+//   console.log(result * discount);
+// }
+
+// const res = convert(500, usdCurr);
+// promotion(res);
+
+// function test() {
+//   for (let i = 0; i < 5; i++) {
+//     console.log(i);
+//     if (i === 3) return;
+//   }
+//   console.log('Done');
+// }
+
+// test();
+
+// function doNothing() {}
+// console.log(doNothing() === undefined);
+
+// Задание на работу с функциями
+
+// Задачи:
+
+// 1) Создайте функцию, которая будет на вход принимать 1 аргумент с именем человека и возвращать строку.
+
+// function sayHello(personName) {
+//   return `Привет, ${personName}!`;
+// }
+// console.log(sayHello('vasya'));
+// console.log(sayHello('Lena'));
+
+// Пример: вызов функции sayHello('Антон') возвращает строку "Привет, Антон!". В решении вызывать функцию не нужно, программа сделает это за вас.
+
+// P.S. возвращать - это использовать ключевое слово return.
+
+// P.S.S. Это классическая функция-модификатор, которых мы дальше по курсу будем создавать еще много в разных видах.
+
+// 2) Создайте функцию, которая принимает в себя 1 аргумент в виде целого числа и возвращает массив из трех чисел: одно на 1 меньше, сам аргумент, и число на 1 больше.
+
+// function returnNeighboringNumbers(num) {
+//   const arr = [];
+//   arr.push(num - 1);
+//   arr.push(num);
+//   arr.push(num + 1);
+//   return arr;
+// }
+
+// console.log(returnNeighboringNumbers(5));
+// console.log(returnNeighboringNumbers(1));
+// console.log(returnNeighboringNumbers(10));
+// Пример: вызов функции returnNeighboringNumbers(5) возвращает массив в виде [4, 5, 6].
+
+// 3) Создайте функцию, которая будет принимать в себя 2 аргумента, оба числа. Первое число - это база, второе число - это сколько раз нужно будет повторить это число в прогрессии. (Смотри пример ниже). Функция должна возвращать строку (или число в особых случаях, о которых ниже), где эти числа идут по порядку, разделенные тремя дефисами "---". После последнего числа их не должно быть.
+
+// Если второй аргумент не является числом, равен или меньше нуля - то возвращать просто первый аргумент. (Проверяем через оператор typeof)
+
+// function getMathResult(num, times) {
+//   if (typeof times !== 'number' || times <= 0) {
+//     return num;
+//   }
+
+//   let str = '';
+
+//   for (let i = 1; i <= times; i++) {
+//     if (i === times) {
+//       str += `${num * i}`;
+//       // Тут без черточек в конце
+//     } else {
+//       str += `${num * i}---`;
+//       // Это тоже самое, что и
+//       // str = str + num * i + "---"
+//     }
+//   }
+
+//   return str;
+// }
+
+// Примеры:
+
+// Вызов функции getMathResult(5, 3) даст ответ 5---10---15
+
+// Вызов функции getMathResult(3, 10) даст ответ 3---6---9---12---15---18---21---24---27---30
+
+// Вызов функции getMathResult(10, 5) даст ответ 10---20---30---40---50
+
+// Вызов функции getMathResult(10, '5') даст ответ 10
+
+// Вызов функции getMathResult(10, 0) даст ответ 10
+
+// Вызов функции getMathResult(20, -5) даст ответ 20
+
+// Эта задача уже ближе к реальности, когда вам нужно учитывать и тип данных у аргументов, проверять их и продумывать логику работы внутри. Обратите внимание на прогрессию, она рассчитывается по простой формуле умножения. Если первый аргумент 5, а второй 3, то число повторяется 3 раза, каждый раз увеличиваясь на само себя. Это базовая математика, которая и нужна для работы в 95% случае на фронтенде.
+
+// Практика ч.3, используем функции
+
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -33,12 +443,49 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    const b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('done !');
+    } else {
+      console.log('error !');
+      i--;
+    }
+  }
+}
 
-console.log(personalMovieDB);
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+  } else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+  } else {
+    console.log('Произошла ошибка');
+  }
+}
+
+detectPersonalLevel();
+
+function showMyDB() {
+  if (personalMovieDB.privat === false) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+  }
+}
+
+writeYourGenres();
